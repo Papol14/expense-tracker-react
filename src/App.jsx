566b9efd3bb2b9afd069afee36038b0b2116eafd
Papol14 <johnpaulescalona2@gmail.com>
2025-01-15 22:09:27 +0800

    Implemented callback

--- src/App.jsx
+++ src/App.jsx
@@ -6,9 +6,29 @@ import TransactionForm from "./components/TransactionForm";
 
 const App = () => {
   const [transactions, setTransactions] = useState([]);
+  const [editingTransaction, setEditingTransaction] = useState(null);
 
   const addTransaction = (transaction) => {
-    setTransactions([transaction, ...transactions]);
+    if (editingTransaction) {
+      // Update existing transaction
+      setTransactions(
+        transactions.map((t) =>
+          t.id === editingTransaction.id ? transaction : t
+        )
+      );
+      setEditingTransaction(null);
+    } else {
+      // Add new transaction
+      setTransactions([transaction, ...transactions]);
+    }
+  };
+
+  const deleteTransaction = (id) => {
+    setTransactions(transactions.filter((t) => t.id !== id));
+  };
+
+  const editTransaction = (transaction) => {
+    setEditingTransaction(transaction);
   };
 
   const getBalance = () => {
@@ -19,8 +39,15 @@ const App = () => {
     <div className="bg-yellow-200 max-w-xl mx-auto p-4 rounded-md">
       <Header />
       <Balance balance={getBalance()} />
-      <History transactions={transactions} />
-      <TransactionForm addTransaction={addTransaction} />
+      <History 
+        transactions={transactions} 
+        onDelete={deleteTransaction}
+        onEdit={editTransaction}
+      />
+      <TransactionForm 
+        addTransaction={addTransaction} 
+        editingTransaction={editingTransaction}
+      />
     </div>
   );
 };
